1b7b58130a25e1c3bdf0c973d21481161b7818cc
johanssof <sofie.johansson96@hotmail.com>
2022-09-26 17:15:45 +0200

    enemy healthbar wip

--- Assets/Scripts/Enemy/EnemyHit.cs
+++ Assets/Scripts/Enemy/EnemyHit.cs
@@ -2,12 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHit : MonoBehaviour
 {
     public Animator anim;
     public GameEvent onBulletHitEnemy;
-    public AmmoData EnemyHealth; 
+    public AmmoData EnemyHealth;
+    public Slider sliderEnemyHealth;
+
+    private void Start()
+    {
+        sliderEnemyHealth.maxValue = EnemyHealth.ammoAmount;
+        sliderEnemyHealth.value = sliderEnemyHealth.maxValue;
+    }
+
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Bullet"))
@@ -21,5 +30,6 @@ public class EnemyHit : MonoBehaviour
     {
         anim.SetTrigger("Hit");
         EnemyHealth.ammoAmount--;
+        sliderEnemyHealth.value = EnemyHealth.ammoAmount;
     }
 }
