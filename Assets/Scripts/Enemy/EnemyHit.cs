ae2eccb82532626280c98632f3d6f429786f4faf
johanssof <sofie.johansson96@hotmail.com>
2022-09-26 16:22:47 +0200

    handledning

--- Assets/Scripts/Enemy/EnemyHit.cs
+++ Assets/Scripts/Enemy/EnemyHit.cs
@@ -6,12 +6,20 @@ using UnityEngine;
 public class EnemyHit : MonoBehaviour
 {
     public Animator anim;
+    public GameEvent onBulletHitEnemy;
+    public AmmoData EnemyHealth; 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Bullet"))
         {
-            //event här! Animation, enemy hälsa ner, kanske ljud, bullet träff-animation, delete bullet
-            anim.SetTrigger("Hit");
+            onBulletHitEnemy.TriggerEvent();
         }
+    }
+
+
+    public void bulletHitEnemy()
+    {
+        anim.SetTrigger("Hit");
+        EnemyHealth.ammoAmount--;
     }
 }
